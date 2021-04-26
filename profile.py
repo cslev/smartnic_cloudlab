e4a73d4fd0216bd080367b55ecf3e5fcb17bd279
cslev <cs.lev@gmx.com>
2021-04-26 09:34:25 +0800

    add profile and source;

---
+++ profile.py
@@ -0,0 +1,69 @@
+"""Bluefield2 - This topology uses two Bluefield2 enabled hosts @ Clemson. They are connected back to back with 3 links; 2 of them forced to be 40G to enforce SmartNIC connectivity. The other link is only required as a common 1G interfaces have the machines connected normally, too"""
+
+#
+# NOTE: This code was machine converted. An actual human would not
+#       write code like this!
+#
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
+
+# Create a portal object,
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+
+#### ---------------------------- TOPOLOGY -------------------------------------
+# Node bf1
+node_bf1 = request.RawPC('bf1')
+node_bf1.hardware_type = 'r7525'
+node_bf1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD'
+iface0 = node_bf1.addInterface('interface-0', pg.IPv4Address('10.0.0.1','255.255.255.0'))
+iface1 = node_bf1.addInterface('interface-2', pg.IPv4Address('10.0.1.1','255.255.255.0'))
+iface2 = node_bf1.addInterface('interface-4', pg.IPv4Address('10.10.10.1','255.255.255.0'))
+
+
+# Node bf2
+node_bf2 = request.RawPC('bf2')
+node_bf2.hardware_type = 'r7525'
+node_bf2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD'
+iface4 = node_bf2.addInterface('interface-1', pg.IPv4Address('10.0.0.2','255.255.255.0'))
+iface5 = node_bf2.addInterface('interface-3', pg.IPv4Address('10.0.1.2','255.255.255.0'))
+iface6 = node_bf2.addInterface('interface-5', pg.IPv4Address('10.10.10.2','255.255.255.0'))
+
+# Link link-0
+link_0 = request.Link('link-0')
+link_0.Site('undefined')
+iface4.bandwidth = 40000000
+link_0.addInterface(iface4)
+iface0.bandwidth = 40000000
+link_0.addInterface(iface0)
+
+# Link link-1
+link_1 = request.Link('link-1')
+link_1.Site('undefined')
+iface1.bandwidth = 40000000
+link_1.addInterface(iface1)
+iface5.bandwidth = 40000000
+link_1.addInterface(iface5)
+
+# Link link-2
+link_2 = request.Link('link-2')
+link_2.Site('undefined')
+iface2.bandwidth = 1000000
+link_2.addInterface(iface2)
+iface6.bandwidth = 1000000
+link_2.addInterface(iface6)
+#### ========================= END TOPOLOGY ====================================
+
+#### ------------------------- CONFIGURE NODES ---------------------------------
+node_bf1.addService(pg.Execute(shell="bash", commad="/local/repository/smartnic_bootstap.sh"))
+
+# Print the generated rspec
+pc.printRequestRSpec(request)
---
+++ smartnic_bootstrap.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+sudo echo -e "\n\n ============ INSTALLATION IS IN PROGRESS =========== " >> /etc/motd
+sudo cat sudo cp /local/repository/source/bashrc_template >> /root/.bashrc
+
+sudo echo -e "\nInstalling MLNX driver..." > /opt/install_log
+#sudo echo -e "\nCopy to /opt..." >> /opt/install_log
+sudo cd /opt
+sudo echo -e "\nDownliading driver to/opt..." >> /opt/install_log
+sudo wget  http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.3-1.0.0.1/MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz >> /opt/install_log
+#sudo cp /local/repository/source/MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz /opt
+#sudo cd /opt
+sudo echo -e "\nUncompress..." >> /opt/install_log
+sudo tar -xzvf MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz
+
+sudo cd MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64
+sudo echo -e "\nInstall driver..." >> /opt/install_log
+sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update --force
+
+
+sudo echo -e "\nEnable openibd" >> /opt/install_log
+sudo /etc/init.d/openibd restart >> /opt/install_log
+
+sudo echo -e "\nEnable rshim" >> /opt/install_log
+sudo systemctl enable rshim
+sudo systemctl start rshim
+sudo systemctl status rshim >> /opt/install_log
+
+
+sudo echo "DISPLAY_LEVEL 1" > /dev/rshim0/misc
+
+sudo echo -e "\nUpdate netplan to assign IP to tmfif_net0..." >> /opt/install_log
+sudo cp /local/repository/source/01-netcfg.yaml /etc/netplan/
+sudo systemctl restart systemd-networkd
+sudo netplan apply
+
+sudo ifconfig tmfifo_net0 >> /opt/install_log
+
+sudo echo -e "\nEnable IP forwarding for the SmartNIC" >> /opt/install_log
+sudo echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+
+sudo echo -e "\n\n ============ DONE =========== " >> /opt/install_log
+sudo echo -e "\n\n ============ INSTALLATION FINISHED =========== " >> /etc/motd
