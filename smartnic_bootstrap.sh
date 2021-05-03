b49496d3a2cc40a4a7857449367060ae132ed837
cslev <cs.lev@gmx.com>
2021-05-03 08:41:57 +0800

    add --ovs-dpdk for mlnx install

--- smartnic_bootstrap.sh
+++ smartnic_bootstrap.sh
@@ -14,7 +14,7 @@ sudo tar -xzvf MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz
 
 cd MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64
 sudo echo -e "\nInstall driver..." |sudo tee -a /opt/install_log
-sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update --force
+sudo ./mlnxofedinstall --auto-add-kernel-support --ovs-dpdk --without-fw-update --force
 
 
 sudo echo -e "\nEnable openibd" |sudo tee -a /opt/install_log
@@ -54,6 +54,21 @@ do
   mlxconfig -d $i q | grep -i internal_cpu
 done
 echo -e "\n\nTo change mode: mlxconfig -d /dev/mst/mt41686_pciconf0 s INTERNAL_CPU_MODEL=1"
+
+
+# FORBIDDEN - HAVE TO BE LOGGED IN :(
+# cd ..
+# sudo echo -e "\nInstall mlnx-dpdk..." |sudo tee -a /opt/install_log
+# wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/mlnx-dpdk_20.11-1mlnx1_amd64.deb
+# wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/mlnx-dpdk-dev_20.11-1mlnx1_amd64.deb
+# DEBIAN_FRONTEND=noninteractive dpkg --force -i mlnx-dpdk_20.11-1mlnx1_amd64.deb
+
+
+# sudo echo -e "\nInstall rxptools and dpi tools..." |sudo tee -a /opt/install_log
+# wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu1804_ubuntu2004/rxp-compiler_21.02.3_amd64.deb
+# wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/rxpbench_21.03_20210401_0_ubuntu_20_amd64.deb
+# wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/doca-dpi-tools_21.03.038-1_amd64.deb
+
 
 
 sudo echo -e "\n\n ============ DONE =========== " |sudo tee -a /opt/install_log
